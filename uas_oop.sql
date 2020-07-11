48dec233f49c7dd88c0603973d8b6b1a5182fe10
alfianbayu11 <49436348+alfianbayu11@users.noreply.github.com>
2020-07-12 06:25:31 +0700

    Add files via upload

---
+++ uas_oop.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Waktu pembuatan: 11 Jul 2020 pada 15.29
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.2.31
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `uas_oop`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tb_mhs`
+--
+
+CREATE TABLE `tb_mhs` (
+  `nim` varchar(11) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `nama` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `jurusan` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `alamat` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data untuk tabel `tb_mhs`
+--
+
+INSERT INTO `tb_mhs` (`nim`, `nama`, `jurusan`, `alamat`) VALUES
+('18090109', 'Alfian Bayu Samudra', 'Teknik Informatika', 'Tegal');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tb_mhs`
+--
+ALTER TABLE `tb_mhs`
+  ADD PRIMARY KEY (`nim`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
